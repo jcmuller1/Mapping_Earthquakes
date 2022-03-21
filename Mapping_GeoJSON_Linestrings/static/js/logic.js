734e5833056bb05e881a85cbb0ba632cfe224e9e
jcmuller1 <muller.jacob@gmail.com>
2022-03-20 19:42:18 -0500

    updating

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -34,10 +34,23 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/jcmuller1/Mapping_Earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3>Airline: " + feature.property.airline + "</h3><hr><h3>Destination: "
+            + feature.properties.dst + "</h3?");
+      }
+  })
+  .addTo(map);
 });
 
